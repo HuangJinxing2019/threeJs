8c721678a1be6968ecd6a4fc8bd6df900270fa95
hjx <731727479@qq.com>
2023-10-11 21:15:03 +0800

    BufferGeometry绘制一个矩形，50个随机三角形

--- demo02/index.js
+++ demo02/index.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from "three/addons/controls/OrbitControls";
+import {BufferAttribute} from "three";
 
 const width = window.innerWidth;
 const height = window.innerHeight;
@@ -10,9 +11,9 @@ renderer.setSize(width, height);
 document.body.appendChild(renderer.domElement);
 
 // 创建相机
-const camera = new THREE.PerspectiveCamera(150, width / height, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
 // 设置相机位置
-camera.position.z = 3;
+camera.position.z = 5;
 
 // 添加轨道控制器
 new OrbitControls(camera, renderer.domElement)
@@ -30,24 +31,39 @@ scene.add(axesHelper)
 const geometry = new THREE.BufferGeometry();
 
 // 创建顶点坐标
-const vertices = new Float32Array([
-    -1.0, -1.0,  1.0,
-    1.0, -1.0,  1.0,
-    1.0,  1.0,  1.0,
+// const vertices = new Float32Array([
+//     -1.0, -1.0,  1.0,
+//     1.0, -1.0,  1.0,
+//     1.0,  1.0,  1.0,
+//
+//     1.0,  1.0,  1.0,
+//     -1.0,  1.0,  1.0,
+//     -1.0, -1.0,  1.0
+// ])
+// // 创建顶点缓冲对象，并复制给几何体对象。
+// geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3))
+//
+// // 创建材质
+// const basicMaterial = new THREE.MeshBasicMaterial({ color: '#ff0000' });
+// // 创建网格对象
+// const cube = new THREE.Mesh(geometry, basicMaterial)
+// // 添加场景
+// scene.add(cube)
 
-    1.0,  1.0,  1.0,
-    -1.0,  1.0,  1.0,
-    -1.0, -1.0,  1.0
-])
-// 创建顶点缓冲对象，并复制给几何体对象。
-geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3))
+// 绘制50个随机三角形
+for (let i = 0; i < 50; i++){
+    const geometry = new THREE.BufferGeometry();
+    const vertices = new Float32Array(9);
+    const basicMaterial = new THREE.MeshBasicMaterial({ transparent: true, opacity: 0.8 });
+    for (let j = 0; j < 9; j ++){
+        vertices[j] = Math.random() * 2;
+    }
+    geometry.setAttribute('position', new BufferAttribute(vertices, 3));
+    basicMaterial.color.setRGB(Math.random(), Math.random(), Math.random());
+    const cube = new THREE.Mesh(geometry, basicMaterial);
+    scene.add(cube)
+}
 
-// 创建材质
-const basicMaterial = new THREE.MeshBasicMaterial({ color: '#ff0000' });
-// 创建网格对象
-const cube = new THREE.Mesh(geometry, basicMaterial)
-// 添加场景
-scene.add(cube)
 // 渲染
 renderer.render(scene, camera)
 
