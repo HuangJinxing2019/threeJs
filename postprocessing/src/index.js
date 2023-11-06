2a3171a18133038f87b2c081d1ba7bb0f0378ca4
huangjinxing <731727479@qq.com>
2023-11-06 15:21:23 +0800

    合成效果，点效果、抗锯齿效果、故障效果、发光效果、自定义合成器

--- postprocessing/src/index.js
+++ postprocessing/src/index.js
@@ -7,6 +7,7 @@ import { DotScreenPass } from "three/addons/postprocessing/DotScreenPass";
 import { SMAAPass } from "three/addons/postprocessing/SMAAPass";
 import { UnrealBloomPass } from "three/addons/postprocessing/UnrealBloomPass";
 import { GlitchPass } from "three/addons/postprocessing/GlitchPass";
+import { ShaderPass } from "three/addons/postprocessing/ShaderPass";
 
 import dat from 'dat.gui';
 
@@ -43,8 +44,87 @@ const smaaPass = new SMAAPass();
 effectComposer.addPass(smaaPass);
 
 // 故障效果
-const glitchPass = new GlitchPass();
-effectComposer.addPass(glitchPass);
+// const glitchPass = new GlitchPass();
+// effectComposer.addPass(glitchPass);
+
+const colorParams = {
+    r: 0,
+    g: 0,
+    b: 0,
+}
+
+const shaderPass = new ShaderPass({
+    uniforms: {
+        tDiffuse: {
+            value: null,
+        },
+        uColor: {
+            value: new THREE.Color(colorParams.r, colorParams.g, colorParams.b)
+        }
+    },
+    vertexShader: `
+        varying vec2 vUv;
+        void main(){
+            vUv = uv;
+            gl_Position = projectionMatrix*modelViewMatrix*vec4(position,1.0);
+        }
+    `,
+    fragmentShader: `
+        varying vec2 vUv;
+        uniform sampler2D tDiffuse;
+        uniform vec3 uColor;
+        void main(){
+            vec4 color = texture2D(tDiffuse,vUv);
+            color.xyz += uColor;
+            gl_FragColor = color;
+        }
+    `
+})
+effectComposer.addPass(shaderPass)
+
+// 自定义着色器通道添加法法线纹理
+const normalTexture = new THREE.TextureLoader().load(new URL('./textures/interfaceNormalMap.png', import.meta.url).href);
+const techPass = new ShaderPass({
+    uniforms: {
+        uTime: {
+            value: 0
+        },
+        tDiffuse: {
+            value: null
+        },
+        uNormalMap: {
+            value: null,
+        },
+    },
+    vertexShader: `
+        varying vec2 vUv;
+        void main (){
+            vUv = uv;
+            gl_Position = projectionMatrix*modelViewMatrix*vec4(position,1.0);
+        }
+    `,
+    fragmentShader: `
+        varying vec2 vUv;
+        uniform sampler2D tDiffuse;
+        uniform sampler2D uNormalMap;
+        uniform float uTime;
+        void main(){
+            vec2 newUv = vUv;
+            newUv += sin(newUv.x * 10.0 + uTime * 2.0) * 0.02;
+            vec4 color = texture2D(tDiffuse, newUv);
+            vec4 normalColor = texture2D(uNormalMap, newUv);
+            // 设置光线的角度
+            vec3 lightDirection = normalize(vec3(-8, 5, 1));
+            float lightness = clamp(dot(normalColor.xyz, lightDirection), 0.0, 1.0);
+            color.xyz += lightness;
+            gl_FragColor = color;
+        }
+    `
+})
+techPass.uniforms.uNormalMap.value = normalTexture;
+effectComposer.addPass(techPass)
+
+
 
 // 发光效果
 const unrealBloomPass = new UnrealBloomPass();
@@ -58,6 +138,18 @@ const gui = new dat.GUI();
 gui.add(unrealBloomPass,'strength').min(0).max(2).step(0.01)
 gui.add(unrealBloomPass,'radius').min(0).max(2).step(0.01)
 gui.add(unrealBloomPass,'threshold').min(0).max(2).step(0.01)
+
+
+gui.add(colorParams,'r').min(-1).max(1).step(0.1).onChange((value) => {
+    shaderPass.uniforms.uColor.value = new THREE.Color(colorParams.r, colorParams.g, colorParams.b)
+})
+gui.add(colorParams,'g').min(-1).max(1).step(0.1).onChange((value) => {
+    shaderPass.uniforms.uColor.value = new THREE.Color(colorParams.r, colorParams.g, colorParams.b)
+})
+gui.add(colorParams,'b').min(-1).max(1).step(0.1).onChange((value) => {
+    shaderPass.uniforms.uColor.value = new THREE.Color(colorParams.r, colorParams.g, colorParams.b)
+})
+
 
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper)
@@ -83,10 +175,11 @@ new GLTFLoader().load(new URL('./models/damagedHelmet.gltf', import.meta.url).hr
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 directionalLight.position.z = 200;
 directionalLight.castShadow = true;
-scene.add(directionalLight)
+scene.add(directionalLight);
 
 function animate(time) {
     requestAnimationFrame(animate)
+    techPass.uniforms.uTime.value = time / 1000;
     effectComposer.render()
 }
 animate()
